fa5fba4dd354b2be4ce93c922f3732e4801a74d3
Jorge Yanez <jyanez11@gmail.com>
2025-12-03 10:28:39 -0500

    coco ejercicios

--- bootcamp frontend/semana05/dia01-01-javascript-basico/main copy.js
+++ bootcamp frontend/semana05/dia01-01-javascript-basico/main copy.js
@@ -1,5 +1,17 @@
 
 
+
+
+
+
+const saludo = 'Hola'
+const nombreCompleto = 'Victor xxxxxxx Villazón'
+const miEdad = 39
+
+// Hola, Victor Villazón. Tienes 39 años.
+console.log(saludo + nombreCompleto + miEdad) // HolaVictor Villazón39 ❓
+console.log(saludo + " " + nombreCompleto + ". Tienes " + miEdad + ' años.')
+
 // 7. (TODO) Almacenar en una constante un número de 3 cifras y mostrar la suma de sus cifras elevada al cubo.
 // Salida: La suma de sus cifras elevada al cubo (13³): 2197
 
@@ -16,6 +28,9 @@ const sumaCifras = centenas + decenas + unidades;
 // 3. Elevar la suma al cubo
 const resultado = sumaCifras ** 3;
 
+console.log(`El número es : ${numero}`);
+console.log( "coco es : " + numero );
+
 // Mostrar el resultado
 console.log(`El número es: ${numero}`);
 console.log(`La suma de sus cifras (${centenas} + ${decenas} + ${unidades}) es: ${sumaCifras}`);
@@ -30,7 +45,7 @@ const montoTotal = 5000;
 
 const porcentajeSocioA = 0.30; // 30%
 const porcentajeSocioB = 0.20; // 20%
-const porcentajeSocioC = 0.60; // 60%
+const porcentajeSocioC = 0.50; // 60%
 
 // --- Cálculos ---
 const parteSocioA = montoTotal * porcentajeSocioA;
@@ -59,7 +74,8 @@ if (totalDistribuido > montoTotal) {
 
 // 1. Determinar si la edad de una persona es mayor de edad o menor de edad mostrando 'Mayor de edad' o 'Menor de edad'
 
-const edadPersona = 39
+const edadPersona = 15
+
 
 if (edadPersona >= 18) {
   console.log('EJERCICIO 1', 'Mayor de edad')
@@ -72,12 +88,42 @@ if (edadPersona >= 18) {
 //    - si es inglés mostrará 'Hello'
 //    - si es aimara mostrará 'kamisaraki'
 
+const lenguaje = "español"
+
+if (lenguaje === "español") {
+  console.log ("Hola")
+} else if (lenguaje === "ingles") {
+   console.log ("Hi")
+} else if (lenguaje === "aimara") {
+   console.log ("kamisaraki")
+} else {
+  console.log ("no conozco tu idioma")
+}
+
+
 // 3. Si un numero es divisible entre 3 y 5 devolver 'fizzbuzz'
 //    Si un numero es divisible entre 3 devolver 'fizz'
 //    Si un numero es divisible entre 5 devolver 'buzz'
 //    De lo contrario devolver el mismo número
 
+const num = 15
+
+if ((num % 3 === 0) & (num % 5 === 0) ) {
+  console.log( 'fizzbuzz')
+} else if (num % 3 === 0) {
+  console.log( "fizz")
+} else if (num % 5 === 0) {
+  console.log('buzz')
+} else {
+  console.log (num)
+}
+
+
 // 4. Retornar true si un numero es primo sino devolver false
 //    Pista: un numero primo es divisible por sí mismo y por 1
+
+const numero1 = 9
+
+if (numero1 / numero1 === 1)
 
 // 5. Devolver un valor que se incremente de dos en dos y devuelva los menores a 10
