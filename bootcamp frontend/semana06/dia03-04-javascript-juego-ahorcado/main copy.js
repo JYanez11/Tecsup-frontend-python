51a14950b896a8ff4f5f01a17a70b11631ceaaa5
Jorge Yanez <jyanez11@gmail.com>
2025-12-24 18:46:43 -0500

    cocotonto

--- bootcamp frontend/semana06/dia03-04-javascript-juego-ahorcado/main copy.js
+++ bootcamp frontend/semana06/dia03-04-javascript-juego-ahorcado/main copy.js
@@ -1,45 +1,70 @@
-const elPalabra = document.querySelector('.ahorcado__palabra')
-const elBotones = document.querySelector('.ahorcado__botones')
-const elResultado = document.querySelector('.ahorcado__resultado')
-const elReiniciar = document.querySelector('.ahorcado__reiniciar')
+const elPalabra = document.querySelector(".ahorcado__palabra");
+const elBotones = document.querySelector(".ahorcado__botones");
+const elResultado = document.querySelector(".ahorcado__resultado");
+const elReiniciar = document.querySelector(".ahorcado__reiniciar");
 
 // elPalabra.textContent = 'HOLA'
 
-const PALABRA_RESPUESTA = 'JAVASCRIPT'
+const PALABRA_RESPUESTA = "JAVA";
 
-let letrasAdivinadas = ''
+let letrasAdivinadas = "";
 
-const ALFABETO = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')  // Arreglo
+// creando el alfabeto
 
+const ALFABETO = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""); // Arreglo
+
+// es solo un bloque
 const renderizarAlfabeto = () => {
-  ALFABETO.forEach(letra => {
-    const boton = document.createElement('button')
+  ALFABETO.forEach((letra) => {
+    const boton = document.createElement("button");
 
-    boton.textContent = letra
+    boton.textContent = letra;
 
-    boton.className = 'bg-blue-500 p-2 text-white font-bold text-2xl cursor-pointer hover:bg-blue-600 duration-300 rounded'
-
-    boton.addEventListener('click', (event) => {
-      letrasAdivinadas += letra
-
-      elPalabra.textContent = PALABRA_RESPUESTA
-        .split('')
-        .map(letra => letrasAdivinadas.includes(letra) ? letra : '_')
-        .join('')
-      
-      if (!elPalabra.textContent.includes('_')) {
-        console.log('GANASTE!')
-        
-        elResultado.classList.toggle('hidden')
-        elReiniciar.classList.toggle('hidden')
-      }
+    boton.className =
+      "boton bg-blue-500 p-2 text-white font-bold text-2xl cursor-pointer hover:bg-blue-600 duration-300 rounded";
+// coco boton
+    
+     // crea botonera en arreglo
+     elBotones.appendChild(boton)
+     
     })
 
-    elBotones.appendChild(boton)
-  })
+    
+  }
 
-}
+  renderizarAlfabeto()
 
-// TODO: Reiniciar el juego al presionar el boton '¡Empezar de nuevo!
+  const boton2 = document.querySelector(".boton");
+  // cocoboton y boton 2
+  
+    boton2.addEventListener("click", (event) => {
+      letrasAdivinadas += letra;
 
-renderizarAlfabeto()
+      elPalabra.textContent = PALABRA_RESPUESTA.split("")
+        .map((letra) => (letrasAdivinadas.includes(letra) ? letra : "#"))
+        .join("");
+      // separa la palabra respuesta para compararla con la letraadivinada e incorprar la letra adivinada
+      // crea '_' para poder colocar el ganaste y el comienza nuevamente, que es complemento del las _ del html
+
+      if (!elPalabra.textContent.includes("#")) {
+        console.log("GANASTE!");
+
+        elResultado.classList.toggle("hidden");
+        elReiniciar.classList.toggle("hidden");
+      }
+    });
+
+   
+ 
+
+  // paso 2- reiniciar
+
+  elReiniciar.addEventListener("click", (event) => {
+    letrasAdivinadas = "";
+    elPalabra.textContent = "___________";
+    elResultado.classList.toggle("hidden");
+    elReiniciar.classList.toggle("hidden");
+  });
+
+
+renderizarAlfabeto();
