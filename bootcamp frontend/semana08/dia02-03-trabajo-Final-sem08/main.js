a7708e896da2eadb71a5a7982e662ffdf51c9bfe
Jorge Yanez <jyanez11@gmail.com>
2026-01-16 09:02:53 -0500

    corazon

--- bootcamp frontend/semana08/dia02-03-trabajo-Final-sem08/main.js
+++ bootcamp frontend/semana08/dia02-03-trabajo-Final-sem08/main.js
@@ -39,9 +39,8 @@ const renderDragon = (dragons = []) => {
         <img src="${dragon.image}" alt="${dragon.name}" width="100">
         <div class="pokemon-item__buttons">
           <button onclick="toggleFavorite(${dragon.id}, '${dragon.name}', '${dragon.image}')">
-            <svg class="${dragon.isFavorite ? 'is-favorite' : ''}" width="24" height="24" viewBox="0 0 24 24" fill="${dragon.isFavorite ? 'gold' : 'none'}" stroke="currentColor" stroke-width="2">
-              <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873l-6.158 -3.245" />
-            </svg>
+            <svg class="${dragon.isFavorite ? 'is-favorite' : '' }" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" class="icon icon-tabler icons-tabler-filled icon-tabler-heart"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6.979 3.074a6 6 0 0 1 4.988 1.425l.037 .033l.034 -.03a6 6 0 0 1 4.733 -1.44l.246 .036a6 6 0 0 1 3.364 10.008l-.18 .185l-.048 .041l-7.45 7.379a1 1 0 0 1 -1.313 .082l-.094 -.082l-7.493 -7.422a6 6 0 0 1 3.176 -10.215z" /></svg>
+                       
           </button>
         </div>
       </article>`;
