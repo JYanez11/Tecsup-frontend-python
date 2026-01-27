795d3ac23a68f30acb09f688ebe464c7b52092c2
Jorge Yanez <jyanez11@gmail.com>
2026-01-27 17:24:16 -0500

    separo componente tarea lista en tarea item

--- bootcamp frontend/semana10/dia01-01-react-todo-app-tailwindcss-componentes/src/components/TareaItem.jsx
+++ bootcamp frontend/semana10/dia01-01-react-todo-app-tailwindcss-componentes/src/components/TareaItem.jsx
@@ -1,9 +1,30 @@
 
 
-function TareaItem() {
+
+const TareaItem = ({ tarea, onCompletado, onRemover }) => {
   return (
-    <div>TareaItem</div>
-  )
-}
+    <li className="flex justify-between">
+      <div className="flex gap-2">
+        <input
+          type="checkbox"
+          onChange={() => onCompletado(tarea.id)}
+          checked={tarea.completado}
+        />
+
+        {/* Aplicamos clase condicional para el tachado */}
+        <span className={tarea.completado ? "line-through text-gray-400" : ""}>
+          {tarea.titulo}
+        </span>
+      </div>
+
+      <button
+        className="cursor-pointer hover:scale-110 transition-transform"
+        onClick={() => onRemover(tarea.id)}
+      >
+        ❌
+      </button>
+    </li>
+  );
+};
 
 export default TareaItem
--- bootcamp frontend/semana10/dia01-01-react-todo-app-tailwindcss-componentes/src/components/TareaLista.jsx
+++ bootcamp frontend/semana10/dia01-01-react-todo-app-tailwindcss-componentes/src/components/TareaLista.jsx
@@ -1,37 +1,19 @@
 // TODO: Crear un componente con solo el LI del UL le llamaremos <TareaItem /> e identificar las propiedades o las funciones que se pasarán al componente.
-
+import TareaItem from "./TareaItem";
 
 const TareaLista = ({ tareas, onCompletado, onRemover }) => {
   return (
     <ul className="flex flex-col gap-2 p-4">
-      {tareas.map(tarea => {
-        return (
-          <li className="flex justify-between" key={tarea.id}>
-            <div className="flex gap-2">
-              <input
-                type="checkbox"
-                onChange={() => onCompletado(tarea.id)}
-                checked={tarea.completado}
-              />
-
-              {/* TODO: la clase line-through solo debe añadirse al className si tarea.completado es true */}
-
-              <span className={ tarea.completado ? "line-through text-red-500" : '' }>
-                {tarea.titulo}
-              </span>
-            </div>
-
-            <button
-              className="cursor-pointer"
-              onClick={() => onRemover(tarea.id)}
-            >
-              ❌
-            </button>
-          </li>
-        )
-      })}
+      {tareas.map((tarea) => (
+        <TareaItem
+          key={tarea.id}
+          tarea={tarea}
+          onCompletado={onCompletado} 
+          onRemover={onRemover}
+        />
+      ))}
     </ul>
-  )
-}
+  );
+};
 
 export default TareaLista
